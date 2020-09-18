adeb1b4c7ef559bc83d29eb8e76b9834213deb3a
Kyungjoon Park <kyungjoon.park@cre.ma>
2020-09-18 15:47:35 +0900

    ADD - 기본 예약 페이지 업로드

---
+++ gi-booking-page/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <UserInput v-on:deliverShowValue="changeName"></UserInput>
+    <Print v-bind:propsdata = "show"></Print>
+  </div>
+</template>
+
+<script>
+import Input from './components/Input.vue'
+import Print from './components/Print.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'UserInput':Input,
+    'Print':Print
+  },
+  data: function() {
+    return {
+      show: true
+    }
+  },
+  methods: {
+		changeName: function() {
+			this.show = !this.show;
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
